ba5ca699948c3d1a22085ea8e02f793451d43231
Lucas Araújo <lucasaraujodev.contact@gmail.com>
2022-04-29 19:38:14 -0300

    finished

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function toggle() {
+    let toggleIcon = document.querySelector(".ico01").addEventListener("mouseover", active);
+    function active() {
+        let wrapper = document.querySelector(".wrapper");
+        wrapper.classList.toggle("active");
+    }
+}
+toggle();
